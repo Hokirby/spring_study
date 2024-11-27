0ec62dd884ae53eb4f08606950923678e6d24e19
HoKirby <lakevely27@gmail.com>
2024-11-27 14:06:16 +0900

    spring study

--- sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -18,12 +18,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class SbbApplicationTests {
-
-	@Autowired
+    /*
+	Autowired
 	private QuestionRepository questionRepository;
-
 	@Autowired
 	private AnswerRepository answerRepository;
+	*/
 
 	@Autowired
 	private QuestionService questionService;
@@ -153,6 +153,16 @@ class SbbApplicationTests {
 
 		assertEquals(1, answerList.size());
 		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+	}*/
+
+	/*
+	@Test
+	void testJpa() {
+		for (int i = 1; i <= 300; i++) {
+			String subject = String.format("테스트 데이터입니다:[%03d]", i);
+			String content = "내용무";
+			this.questionService.create(subject, content);
+		}
 	}*/
 
 	@Test
